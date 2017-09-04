f4df47b5fc71f076396f39b53cd35c5902c9e173
Cody Boyko <codywboyko@gmail.com>
2017-09-04 12:40:20 +0000

    Shelf selected to show on search results as it changes.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
-import debounce from 'lodash/debounce'
 
 class SearchBooks extends Component {
   state = {
@@ -18,61 +17,61 @@ class SearchBooks extends Component {
     this.search(query)
   }
 
-  search = debounce((query) => {
+  search = (query) => {
     if (query) {
       BooksAPI.search(query).then((results) => {
         // Without this guard it wont let the user clear search results.
         if (results.length) {
-          console.log(results)
           this.setState({ results: results })
         } else {
           this.clearResults()
         }
       })
     }
-  }, 250)
+  }
 
   clearResults = () => {
     this.setState({ results: [] })
   }
   
   updateBookShelf = (id, shelf) => {
-    // set state
-    // this.setState((state) => ({
-    //   books: state.books.find((c) => c.id === id).shelf = shelf
-    // }))
-    // console.log(this.state.books.find((c) => c.id === id))
-    // // update book
-    // #update() does return my shelf that I'm adding a book to.
-    // Should be able to set that as a state. Maybe when I first get books state
-    // I can filter it and fill my other states, then use this to update states 
-    // as the user updates them.
-    BooksAPI.update(this.state.results.find((c) => c.id === id), shelf)
+    let modifiedResults = this.state.results
+    let book = modifiedResults.find((c) => c.id === id)
+    book.shelf = shelf
+    // Set the result state.
+    this.setState((state) => ({
+      results: modifiedResults
+    }))
+    // Update server.
+    BooksAPI.update(book, shelf)
+    this.getMyBooks()
   }
 
   // Get all of the books for the shelves.
   componentDidMount() {
+    this.getMyBooks()
+  }
+  
+  getMyBooks = () => {
     BooksAPI.getAll().then((myBooks) => {
       this.setState({myBooks})
     })
   }
 
   render() {
-    // Map over the results and the myBooks and create resultsWithShelf like
-    // in the contacts app.
-    // The else block setting to none overrode this I think. Also, this is updating the result and creating a
-    // 2d array for resultswithshelf
-    let resultsWithShelf = []
     // Map over results and for each result map over myBooks to find matches.
-    resultsWithShelf = this.state.results.map(result => this.state.myBooks.map(myBook => {
-      if(result.id === myBook.id){
-        result.shelf = myBook.shelf
-        return result
-      } 
-    }))
-    
-    console.log(resultsWithShelf)
-    
+    this.state.results.map(result => {
+      // Set default shelf to none.
+      result.shelf = 'none'
+      this.state.myBooks.forEach(myBook => {
+        // Update shelf as needed.
+        if(result.id === myBook.id){
+          result.shelf = myBook.shelf
+        } 
+      })
+      return result
+    })
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
